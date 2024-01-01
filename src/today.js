9ed9106f7ab5dc4ba00beb5df6ada6dc1c6b7e84
Josue-Caballero-Sanchez <sanchez684.jc@gmail.com>
2024-01-01 11:00:36 -0700

    Fixed today tab not working on single digit days

--- src/today.js
+++ src/today.js
@@ -9,8 +9,17 @@ export function loadToday(){
     container.appendChild(title);
 
     let today = new Date().toLocaleDateString();
+    let splitToday = today.split("/");
+    if(splitToday[0].length === 1){
+        splitToday[0] = "0" + splitToday[0];
+    }
+    if(splitToday[1].length === 1){
+        splitToday[1] = "0" + splitToday[1];
+    }
+
+    today = splitToday[0] + "/" + splitToday[1] + "/" + splitToday[2];
+
     for(let i = 0; i < itemsArray.length; i++){
-        
 
         if(itemsArray[i].date === today){
             printTask(i, itemsArray[i]);
